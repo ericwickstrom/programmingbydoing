0226ac190cb507f01de5f6e2c537975d9f115b77
Eric Wickstrom <wickstrome@gmail.com>
2016-08-12 18:50:39 -0500

    fizzbuzz complete

---
+++ src/com/beardsmcgee/programmingbydoing/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.beardsmcgee.programmingbydoing;
+
+/*
+ * Write a program that prints the numbers from 1 to 100. 
+ * But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+ * For numbers which are multiples of both three and five print "FizzBuzz".
+ */
+
+public class FizzBuzz {
+	private static final String FIZZ = "FIZZ";
+	private static final String BUZZ = "BUZZ";
+	private static final String SPACE = " ";
+	
+	public static final void main(String[] args){
+		for(int i = 1; i <= 100; i++){
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.valueOf(i));
+			sb.append(SPACE);
+			if(i % 3 == 0){
+				sb.append(FIZZ);
+			}
+			if(i % 5 == 0){
+				sb.append(BUZZ);
+			}
+			System.out.println(sb.toString());
+		}
+	}
+}
